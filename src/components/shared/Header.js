b45f52e3112fa8e86063cb4e26b334d7cafab8a1
Chi Nguyen <chi.nguyen@live.com>
2023-02-05 13:45:19 +0200

    add searchbar to header

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -6,24 +6,29 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
 function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <Link to="/">
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Link>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Country
           </Typography>
+          <SearchBar />
         </Toolbar>
       </AppBar>
     </Box>
